bd2626b2468299c518d9e7a264ab13796def912a
5zdob <abdalahfzaz44@gmail.com>
2023-09-07 04:17:22 -0700

    fast work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful\" Dora Korpar, 2015-310-19\n",
+		59);
+
 	return (1);
 }
